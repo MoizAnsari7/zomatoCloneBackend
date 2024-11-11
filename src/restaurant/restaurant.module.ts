a4179f582647643c54d8e579b27aa36398c26f30
MoizAnsari7 <moizansari176@gmail.com>
2024-11-11 15:06:26 +0530

    Modified Modules

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,8 +1,12 @@
+// src/restaurant/restaurant.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantController } from './restaurant.controller';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Restaurant])],
   controllers: [RestaurantController],
   providers: [RestaurantService],
 })
