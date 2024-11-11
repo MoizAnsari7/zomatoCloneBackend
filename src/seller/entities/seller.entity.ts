16366b1c3344dd5d8a13f14d10fd8543bd922946
MoizAnsari7 <moizansari176@gmail.com>
2024-11-11 16:46:54 +0530

    Seller Entity Added

--- src/seller/entities/seller.entity.ts
+++ src/seller/entities/seller.entity.ts
@@ -1,1 +1,20 @@
-export class Seller {}
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Restaurant } from '../restaurant/restaurant.entity';
+
+@Entity()
+export class Seller {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Restaurant, (restaurant) => restaurant.seller)
+  restaurants: Restaurant[];
+}
