237816cff92a9bd87d97d4a7d90e7fdde256aad8
MoizAnsari7 <moizansari176@gmail.com>
2024-11-11 14:20:36 +0530

    User Create DTO Added

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+// src/user/dto/create-user.dto.ts
+import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
+  password: string;
+}
