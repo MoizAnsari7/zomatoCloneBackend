c605bcec93a5b3aba7797e856e2c22223c2d22db
MoizAnsari7 <moizansari176@gmail.com>
2024-11-11 16:19:03 +0530

    Review Entity Added

--- src/review/entities/review.entity.ts
+++ src/review/entities/review.entity.ts
@@ -1,1 +1,21 @@
-export class Review {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Restaurant } from '../restaurant/restaurant.entity';
+
+@Entity()
+export class Review {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  comment: string;
+
+  @Column('int')
+  rating: number;
+
+  @ManyToOne(() => User, (user) => user.reviews, { eager: true })
+  user: User;
+
+  @ManyToOne(() => Restaurant, (restaurant) => restaurant.reviews, { eager: true })
+  restaurant: Restaurant;
+}
